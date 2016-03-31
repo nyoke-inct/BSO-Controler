7110945b25a97c11b20ded0e73c4a0f741f78d9d
Yoshikazu Kawayoke <nyoke.com@gmail.com>
2016-03-31 11:50:48 +0900

    Initial Commit

---
+++ BSOControler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BSOControler
+//
+//  Created by KawayokeYoshikazu on 2016/03/31.
+//  Copyright © 2016年 Yoshikazu Kawayoke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BSOControlerTests/BSOControlerTests.swift
@@ -0,0 +1,36 @@
+//
+//  BSOControlerTests.swift
+//  BSOControlerTests
+//
+//  Created by KawayokeYoshikazu on 2016/03/31.
+//  Copyright © 2016年 Yoshikazu Kawayoke. All rights reserved.
+//
+
+import XCTest
+@testable import BSOControler
+
+class BSOControlerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
